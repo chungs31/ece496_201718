9c59f86ad94618dc1affd8fcccbe6fdbab433875
Seung-Hun Chung <sh.c@live.ca>
2017-10-03 22:52:18 -0400

    removed a bunch of stuff

---
+++ vivado_proj/vivado-essentials/fifo_32x512/fifo_32x512_stub.vhdl
@@ -0,0 +1,37 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.2.1 (win64) Build 1957588 Wed Aug  9 16:32:24 MDT 2017
+-- Date        : Mon Oct  2 23:39:40 2017
+-- Host        : DESKTOP-UA5FUC7 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               C:/xillinux-eval-zedboard-2.0a/vivado-essentials/fifo_32x512/fifo_32x512_stub.vhdl
+-- Design      : fifo_32x512
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity fifo_32x512 is
+  Port ( 
+    clk : in STD_LOGIC;
+    srst : in STD_LOGIC;
+    din : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    wr_en : in STD_LOGIC;
+    rd_en : in STD_LOGIC;
+    dout : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    full : out STD_LOGIC;
+    empty : out STD_LOGIC
+  );
+
+end fifo_32x512;
+
+architecture stub of fifo_32x512 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,srst,din[31:0],wr_en,rd_en,dout[31:0],full,empty";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "fifo_generator_v13_1_4,Vivado 2017.2.1";
+begin
+end;
