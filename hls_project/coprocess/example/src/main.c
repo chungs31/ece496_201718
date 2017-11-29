8615e76850f913a5a330b22f868548bae6f610e3
Matt Agostini <matt.agostini95@gmail.com>
2017-11-28 22:38:12 -0500

    Matrix Multiply in HLS
    Adding code for matrix multiplication on the FPGA

--- hls_project/coprocess/example/src/main.c
+++ hls_project/coprocess/example/src/main.c
@@ -9,7 +9,24 @@ int mycalc(int a, float *x2) {
   return a + 1;
 }
 
-void xillybus_wrapper(int *in, int *out) {
+void matrixMultiply(float* A, float* B, float* C)
+{
+ // matrix multiplication of a A*B matrix
+	L1:for (int row = 0; row < 4; row++)
+	{
+		L2:for (int col = 0; col < 3; col++)
+		{
+			float sum = 0;
+			L3:for (int z = 0; z < 2; z++)
+			{
+				sum += A[row * 2 + z] * B[z*3 + col];
+			}
+			C[row * 4 + col] = sum;
+		}
+	}
+}
+
+void xillybus_wrapper(float *in, float *out) {
 #pragma AP interface ap_fifo port=in
 #pragma AP interface ap_fifo port=out
 #pragma AP interface ap_ctrl_none port=return
@@ -17,24 +34,37 @@ void xillybus_wrapper(int *in, int *out) {
   uint32_t x1, tmp, y1;
   float x2, y2;
 
-  xilly_puts("Hello, world\n");
+  float A[8];
+  float B[6];
+  float C[12];
+
+  xilly_puts("Let's Matrix Multiply\n");
+
+	for (int i = 0; i < 8; i++)
+	{
+		A[i] = *in++;
+	}
+
+	for (int j = 0; j < 6; j++)
+    {
+		B[j] = *in++;
+    }
 
   // Handle input data
-  x1 = *in++;
-  tmp = *in++;
-  x2 = *((float *) &tmp); // Convert uint32_t to float
+  //x1 = *in++;
+  //tmp = *in++;
 
   // Debug output
-  xilly_puts("x1=");
-  xilly_decprint(x1, 1);
-  xilly_puts("\n");
+  //xilly_puts("x1=");
+  //xilly_decprint(x1, 1);
+  //xilly_puts("\n");
 
-  // Run the calculations
-  y1 = mycalc(x1, &x2);
-  y2 = x2; // This helps HLS in the conversion below
+  matrixMultiply(A, B, C);
 
-  // Handle output data
-  tmp = *((uint32_t *) &y2); // Convert float to uint32_t
-  *out++ = y1;
+  	for (int k = 0; k < 12; k++)
+  	{
+  		*out++ = C[k];
+  	}
+
   *out++ = tmp;
 }
