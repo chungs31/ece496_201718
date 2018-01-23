3510b4b4a1e9ac69e415da9e66c507e0810cd671
Matt Agostini <matt.agostini95@gmail.com>
2018-01-23 00:03:19 -0500

    Adding variable length MM

--- hls_project/coprocess/example/src/main.c
+++ hls_project/coprocess/example/src/main.c
@@ -9,19 +9,19 @@ int mycalc(int a, float *x2) {
   return a + 1;
 }
 
-void matrixMultiply(float* A, float* B, float* C)
+void matrixMultiply(float* A, float* B, float* C, int dim1, int dim2, int dim3)
 {
  // matrix multiplication of a A*B matrix
-	L1:for (int row = 0; row < 4; row++)
+	L1:for (int row = 0; row < dim1; row++)
 	{
-		L2:for (int col = 0; col < 3; col++)
+		L2:for (int col = 0; col < dim3; col++)
 		{
 			float sum = 0;
-			L3:for (int z = 0; z < 2; z++)
+			L3:for (int z = 0; z < dim2; z++)
 			{
-				sum += A[row * 2 + z] * B[z*3 + col];
+				sum += A[row * dim2 + z] * B[z*dim3 + col];
 			}
-			C[row * 4 + col] = sum;
+			C[row * dim3 + col] = sum;
 		}
 	}
 }
@@ -34,35 +34,38 @@ void xillybus_wrapper(float *in, float *out) {
   uint32_t x1, tmp, y1;
   float x2, y2;
 
-  float A[8];
-  float B[6];
-  float C[12];
+  int dim1;
+  int dim2;
+  int dim3;
+
+  dim1 = *in++;
+  dim2 = *in++;
+  dim3 = *in++;
+
+  float A[10240];
+  float B[10240];
+  float C[10240];
 
   xilly_puts("Let's Matrix Multiply\n");
 
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < dim1*dim2; i++)
 	{
 		A[i] = *in++;
 	}
 
-	for (int j = 0; j < 6; j++)
+	for (int j = 0; j < dim2*dim3; j++)
     {
 		B[j] = *in++;
     }
 
-  // Handle input data
-  //x1 = *in++;
-  //tmp = *in++;
+  matrixMultiply(A, B, C, dim1, dim2, dim3);
 
-  // Debug output
-  //xilly_puts("x1=");
-  //xilly_decprint(x1, 1);
-  //xilly_puts("\n");
-
-  matrixMultiply(A, B, C);
-
-  	for (int k = 0; k < 12; k++)
+  	for (int k = 0; k < dim1*dim3; k++)
   	{
+  		xilly_puts("C is");
+  		xilly_decprint(C[k], 2);
+  		xilly_puts("\n");
+
   		*out++ = C[k];
   	}
 
